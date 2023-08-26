c6b12725f23923024a67425e96faad61f6478332
KevinLmn <ikebilol@gmail.com>
2023-08-26 19:20:35 +0200

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import React, { useEffect, useState } from "react";
+
+const Index = () => {
+  const [count, setCount] = useState<number>(0);
+
+  return <form></form>;
+};
+
+export default Index;
